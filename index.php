62b635ea14aebbdb95b6a4c3878c8cba5b9ac927
Dhellice <louise.danicourt@hotmail.fr>
2016-10-04 16:31:31 +0200

    bootstrap.min

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
 		<title>CocktailsMania</title>
 		<meta charset="UTF-8">
 
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+		
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
 
