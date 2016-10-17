3b151847a2c1d31cdb89d800de8b3e8af0d33cba
Aurélien Boncenne <aurelien.boncenne@gmail.com>
2016-10-17 11:03:50 +0200

    fonction recette qui join le tableau de selection du Select

--- index.php
+++ index.php
@@ -1,97 +1,52 @@
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
 		<title>CocktailsMania</title>
 		<meta charset="UTF-8">
-		<script src="js/script.js"></script>
+		
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
-	</head>
-	<body>
-    <nav class="navbar navbar-inverse navbar-default navbar-static-top">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Cocktails</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Ingredients<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Recette</a></li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-  <div class="recette">
+</head>
+<body>
+    <div class="recette">
     <form method="post">
-      <br>
-    <h1>Recette</h1>
-      <input id="recette" type="textarea" name="content" placeholder="content">
-      <br>
-      <input type="button" value="Ok" onclick="ajout_recette()">
-        <input type="button" value="supprime" onclick="supprime_recette()">
       <h1>Ingrédients</h1>
-      <input id="igredients" type="textarea" name="content" placeholder="content">
+        <input id="igredients" type="textarea" name="content" placeholder="content">
       <br>
         <input type="button" value="Ok" onclick="ajout_ingredient()">
         <input type="button" value="supprime" onclick="supprime_ingredient()">
-        <select class="selection" onclick="newstab()">
-        <option></option>
-        </select>
     </form>
   </div>
-<div class="recette2">
-    <h2>Recette</h2>
-    <ul id="recette1">
-        <li>Pouet</li>
-    </ul>
+    <div class="recette2">
     <h2>Ingredients</h2>
     <ul id="ingredient1">
-        <li>lol</li>
     </ul>
-</div>
-
-<footer class="footer-distributed">
-
-      <div class="footer-left">
-
-        <p class="footer-links">
-          <a href="#">Contact</a>
-        </p>
-
-        <p class="footer-company-name">CocktailsMania</p>
-
-      </div>
-
-      <div class="footer-right">
-
-        <form method="get" action="#">
-          <input placeholder="Search our website" name="search" />
-          <i class="fa fa-search"></i>
-        </form>
-
-      </div>
-<script src="js/jquery-latest.js"></script>
-<script src="js/bootstrap.min.js"></script>
-    </footer>
-	</body>
-	</html>
+        </div>
+    <div class="row"></div>
+    <div class="recette">
+    <form method="post">
+      <br>
+        <h1>Recette</h1>
+      <input id="recette" type="textarea" name="content" placeholder="content">
+      <br>
+        <input type="button" value="Ok" onclick="ajout_recette()">
+        <input type="button" value="supprime" onclick="supprime_recette()">
+        <br>
+        <select id="Select">
+        </select>
+        <input type="button" value="add" onclick="add_selection()">
+    </form>
+  </div>
+    <div class="recette2">
+        <h2>Recette</h2>
+        <ul id="recette1">
+        </ul>
+            <h2>Les ingrédients choisi</h2>
+        <ul id="selection1">
+            <li>Du Curry</li>
+        </ul>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,52 +1,65 @@
 // Ajout/supprime ingrédient recette ajout tab for ajout li
-
-var ingredients = [];
-console.log(ingredients.length);
+var ingredients = ["rhum", "jus de citron", "vin rouge"]
+for (var i = 0; i < ingredients.length; i++) {
+    console.log(ingredients[i]);
+    liste = document.getElementById("ingredient1");
+    nouveauli = document.createElement("li");
+    nouveauli.innerHTML = ingredients[i];
+    liste.appendChild(nouveauli);
+}
 var recette = [];
-console.log(recette.lenght);
-var mix =[];
-console.log(mix.length);
+var selection = [];
+function ajout_recette() {
+    liste = document.getElementById("recette1");
+    titre = document.getElementById("recette").value;
+    recette[recette.length] = titre;
+    tab = recette.join(selection);
+    console.log(recette);
+    nouveauli = document.createElement("li");
+    nouveauli.innerHTML = tab;
+    liste.appendChild(nouveauli);
+}
 
-	function ajout_recette()
-    {
-            liste = document.getElementById("recette1");
-            texte = document.getElementById("recette").value;
-            recette[recette.length] = texte;
-            console.log(recette);
-            nouveauli = document.createElement("li");
-            nouveauli.innerHTML = texte;
-            liste.appendChild(nouveauli);
-            
-		}
-    function supprime_recette()
-    {
-        liste = document.getElementById("recette1");
-        if (document.getElementsByTagName("li").length > 0)
-            {
-                liste.removeChild(liste.lastChild);
-            }
+function supprime_recette() {
+    liste = document.getElementById("recette1");
+    if (document.getElementsByTagName("li").length > 0) {
+        liste.removeChild(liste.lastChild);
     }
-	function ajout_ingredient()
-		{
-            liste = document.getElementById("ingredient1");
-            texte = document.getElementById("igredients").value;
-            ingredients[ingredients.length] = texte;
-            console.log(ingredients);
-            nouveauli = document.createElement("li");
-            nouveauli.innerHTML = texte;
-            liste.appendChild(nouveauli);
-            
-		}
-    function supprime_ingredient()
-    {
-        liste = document.getElementById("ingredient1");
-        if (document.getElementsByTagName("li").length > 0)
-            {
-                liste.removeChild(liste.lastChild);
-            }
+}
+
+function ajout_ingredient() {
+    liste = document.getElementById("ingredient1");
+    texte = document.getElementById("igredients").value;
+    ingredients[ingredients.length] = texte;
+    console.log(ingredients);
+    nouveauli = document.createElement("li");
+    nouveauli.innerHTML = texte;
+    liste.appendChild(nouveauli);
+}
+
+function supprime_ingredient() {
+    liste = document.getElementById("ingredient1");
+    if (document.getElementsByTagName("li").length > 0) {
+        liste.removeChild(liste.lastChild);
     }
-    function newstab()
-    {
-        Array.isArray(recette[],ingredients[]);
-        console.log(newstab.lenght)
+}
+
+function add_selection(){
+    liste = document.getElementById("selection1");
+    var x = document.getElementById("Select").selectedIndex;
+    var y = document.getElementById("Select").options;
+    selection[selection.length] = y;
+    console.log(selection);
+    nouveauli = document.createElement("li");
+    nouveauli.innerHTML = y;
+    liste.appendChild(nouveauli);
+}
+function Select() {
+    for (var a = 0; a < ingredients.length; a++) {
+        var x = document.getElementById("Select");
+        var option = document.createElement("option");
+        option.innerHTML = ingredients[a];
+        x.add(option);
     }
+}
+Select();
